b6d38d87ea4bf3c5888e43800d16336103f4d385
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-04-26 05:35:01 +0200

    Add project files.

---
+++ N-puzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace N_puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
