bd9a39e5e7433d69cc01113c042bff2a62b27234
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-05-19 22:49:08 +0200

    Add Node Class

---
+++ N-puzzle/Node.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_puzzle
+{
+    class Node
+    {
+        public int g;
+        public int h;
+        public int M;
+        public int cost;
+        public Node parent;
+        public int[,] state;
+        public char come_from;
+
+
+        public Node(int[,] puzzle, Node parent, int g, char came_from)
+        {
+            this.g = g;
+            this.h = calculateHamming(puzzle);
+            this.M = calculateMenhatten(puzzle);
+            this.cost = this.M + this.g;
+            //this.cost = this.h + this.g;
+            this.parent = parent;
+            this.state = puzzle;
+            this.come_from = came_from;
+        }
+        public void print_state(Node node, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(node.state[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static int calculateHamming(int[,] puzzle)
+        {
+            int hamming = 0;
+            int c = 1;
+            for (int i = 0; i < puzzle.GetLength(0); i++)
+            {
+                for (int j = 0; j < puzzle.GetLength(0); j++)
+                {
+                    if ((i + j + c) == puzzle[i, j] || puzzle[i, j] == 0) { }
+                    else { hamming = hamming + 1; }
+                }
+                c = c + puzzle.GetLength(0) - 1;
+            }
+            return hamming;
+        }
+        static int calculateMenhatten(int[,] puzzle)
+        {
+            int DistanceSum = 0;
+            for (int i = 0; i < puzzle.GetLength(0); i++)
+            {
+                for (int j = 0; j < puzzle.GetLength(0); j++)
+                {
+                    if (puzzle[i, j] != 0)
+                    {
+                        int x = (puzzle[i, j] - 1) / puzzle.GetLength(0);
+                        int y = (puzzle[i, j] - 1) % puzzle.GetLength(0);
+                        int dx;
+                        if (x > -i)
+                            dx = x - i;
+                        else
+                            dx = i - x;
+                        int dy;
+                        if (y > j)
+                            dy = y - j;
+                        else
+                            dy = j - y;
+                        DistanceSum += Math.Abs(dx) + Math.Abs(dy);
+                    }
+                }
+            }
+            return DistanceSum;
+        }
+    }
+}
--- N-puzzle/Program.cs
+++ N-puzzle/Program.cs
@@ -5,6 +5,68 @@ namespace N_puzzle
 {
     class Program
     {
+        static bool goal(int[,] puzzle, int size)
+        {
+            int goal = 1;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (puzzle[i,j] == 0) continue;
+                    else if (puzzle[i,j] != goal++) return false;
+                }
+            }
+            return true;
+        }
+        static int ASTAR(int[,] puzzle, int size)
+        {
+            int g = 0, h = 0;
+            PriorityQueue pq = new PriorityQueue();
+            //pq.Enqueue(calculateMenhatten(puzzle,size));
+            if (goal(puzzle, size))
+            {
+                //Console.WriteLine("\nSolved in %d moves", g);
+            }
+            // NOW WWWHHHHAAAAAAATTTT?????
+            return 0;
+        }
+
+        static int calculateMenhatten(int[,] puzzle,int size)
+        {
+            int DistanceSum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (puzzle[i,j] != 0)
+                    {
+                        int x = (puzzle[i,j] - 1) / size;
+                        int y = (puzzle[i,j] - 1) % size;
+                        int dx = x - i;
+                        int dy = y - j; 
+                        DistanceSum += Math.Abs(dx) + Math.Abs(dy);
+                        //total_distance += ABS(x, i) + ABS(y, j);
+                    }
+                }
+            }
+            return DistanceSum;
+        }
+        static int calculateHamming(int[] puzzle)
+        {
+            int hamming = 0;
+            for(int i = 0; i < puzzle.Length-1; i++)
+            {
+                if(i+1 == puzzle[i] && puzzle[i]!=0)
+                {
+
+                }
+                else
+                {
+                    hamming = hamming + 1;
+                }
+            }
+            return hamming;
+        }
         static bool isSolvable(int[] puzzle,int size,int index)
         {
             int inversions = 0;
@@ -12,8 +74,8 @@ namespace N_puzzle
                 for (int j = i + 1; j < size*size; j++) {
                     if (puzzle[i] != 0 && puzzle[j]!=0 && puzzle[i] > puzzle[j])
                     {
-                        Console.WriteLine(puzzle[i]);
-                        Console.WriteLine(puzzle[j]);
+                        //Console.WriteLine(puzzle[i]);
+                        //Console.WriteLine(puzzle[j]);
                         inversions++;
                     }
                 }
@@ -38,7 +100,7 @@ namespace N_puzzle
             int n;
             StreamReader sr;
             string line;
-            file = new FileStream("50 Puzzle.txt", FileMode.Open, FileAccess.Read);
+            file = new FileStream("8 Puzzle (3).txt", FileMode.Open, FileAccess.Read);
             string[,] puzzle;
             sr = new StreamReader(file);
             line = sr.ReadLine();
@@ -55,8 +117,10 @@ namespace N_puzzle
             {
                 line = sr.ReadLine();
             }
-                int zeroIndex=0;
-            
+            int zeroIndex=0;
+            int zeroJndex = 0;
+
+
             for (int i = 0; i < n; i++)
             {    
                 string[] vertices = line.Split(' ');
@@ -77,6 +141,7 @@ namespace N_puzzle
                     if (puz2d[i, j] == 0)
                     {
                         zeroIndex = i;
+                        zeroJndex = j;
                     }
                     puz1d[k++] = Int32.Parse(puzzle[i, j]);
                 }
@@ -86,7 +151,21 @@ namespace N_puzzle
             else
                 Console.WriteLine("Not Solvable");
 
+            int Hamming = calculateHamming(puz1d);
+            Console.WriteLine(Hamming);
 
+            int Menhatten = calculateMenhatten(puz2d,n);
+            Console.WriteLine(Menhatten);
+            ////////////////////////////////////////////////////
+            //PriorityQueue pq = new PriorityQueue();
+            //pq.Enqueue(10);
+            //pq.Enqueue(2);
+            //pq.Enqueue(5);
+            //pq.Enqueue(6);
+
+            //Console.WriteLine(pq.Peek());
+            //pq.Dequeue();
+            //Console.WriteLine(pq.Peek());
         }
     }
 }
